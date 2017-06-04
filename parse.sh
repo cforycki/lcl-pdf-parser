dc1827b0ddb03d4d1711ac6a7005b852a5aa8a3a
Christophe Forycki <forycki.christophe@gmail.com>
2017-06-04 21:47:34 +0200

    Correction for debit and credit operations

--- parse.sh
+++ parse.sh
@@ -4,8 +4,8 @@ for file in $@
 do
 	OUTPUT=$(gs -dBATCH -dSAFER -q -dNOPAUSE -sDEVICE=txtwrite -sOutputFile=- $file 2>/dev/null)
 	DATES=$(echo "$OUTPUT" | perl -ne '$first != 1 and /du\s*?(\d{2}\.\d{2}\.\d{4})\s*?au\s*?(\s*?\d{2}\.\d{2}\.\d{4}).*/ and print "$1;$2" and $first = 1');
-	DEBITS=$(echo "$OUTPUT"| perl -ne '/^\s*(?:\d{2}\.\d{2})\s*(.*?)\s*(\d{2})\.(\d{2})\.(\d{2})\s{0,18}(\d+(?:\s+\d+)*,\d+)?.*?/ and print "$2-$3-$4;0;;;$1;-$5;;\n"')
-	CREDITS=$(echo "$OUTPUT" | perl -ne '/^\s*(?:\d{2}\.\d{2})\s*(.*?)\s*(\d{2})\.(\d{2})\.(\d{2})\s{19,}(\d+(?:\s+\d+)*,\d+)?.*?/ and print "$2-$3-$4;0;;;$1;$5;;\n"');
+	DEBITS=$(echo "$OUTPUT"| perl -ne '/^\s*(?:\d{2}\.\d{2})\s*(.*?)\s*(\d{2})\.(\d{2})\.(\d{2})\s{0,18}(\d+(?:\s+\d+)*,\d+).*?/ and print "$2-$3-$4;0;;;$1;-$5;;\n"')
+	CREDITS=$(echo "$OUTPUT" | perl -ne '/^\s*(?:\d{2}\.\d{2})\s*(.*?)\s*(\d{2})\.(\d{2})\.(\d{2})\s{19,}(\d+(?:\s+\d+)*,\d+).*?/ and print "$2-$3-$4;0;;;$1;$5;;\n"');
 	ANCIEN_SOLDE=$(echo "$OUTPUT" | perl -ne '/^\s*(?:\d{2}\.\d{2})\s*ANCIEN SOLDE(\s*)(\d+(?:\s+\d+)*,\d+)?.*?/ and print ((length($1) > 18)?"$2":"-$2")');
 	SOLDE=$(echo "$OUTPUT" | perl -ne '/^\s*(?:\d{2}\.\d{2})\s*SOLDE EN EUROS(\s*)(\d+(?:\s+\d+)*,\d+)?.*?/ and print ((length($1) > 18)?"$2":"-$2")');
 
