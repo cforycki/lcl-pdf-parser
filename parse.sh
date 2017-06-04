7e82adb53ed7337c6e8f92d724e37779a7d94e21
Christophe Forycki <forycki.christophe@gmail.com>
2017-06-04 20:30:33 +0200

    Added previous and final balance

--- parse.sh
+++ parse.sh
@@ -6,9 +6,14 @@ do
 	DATES=$(echo "$OUTPUT" | perl -ne '$first != 1 and /du\s*?(\d{2}\.\d{2}\.\d{4})\s*?au\s*?(\s*?\d{2}\.\d{2}\.\d{4}).*/ and print "$1;$2" and $first = 1');
 	DEBITS=$(echo "$OUTPUT"| perl -ne '/^\s*(?:\d{2}\.\d{2})\s*(.*?)\s*(\d{2})\.(\d{2})\.(\d{2})\s{0,18}(\d+(?:\s+\d+)*,\d+)?.*?/ and print "$2-$3-$4;0;;;$1;-$5;;\n"')
 	CREDITS=$(echo "$OUTPUT" | perl -ne '/^\s*(?:\d{2}\.\d{2})\s*(.*?)\s*(\d{2})\.(\d{2})\.(\d{2})\s{19,}(\d+(?:\s+\d+)*,\d+)?.*?/ and print "$2-$3-$4;0;;;$1;$5;;\n"');
+	ANCIEN_SOLDE=$(echo "$OUTPUT" | perl -ne '/^\s*(?:\d{2}\.\d{2})\s*ANCIEN SOLDE(\s*)(\d+(?:\s+\d+)*,\d+)?.*?/ and print ((length($1) > 18)?"$2":"-$2")');
+	SOLDE=$(echo "$OUTPUT" | perl -ne '/^\s*(?:\d{2}\.\d{2})\s*SOLDE EN EUROS(\s*)(\d+(?:\s+\d+)*,\d+)?.*?/ and print ((length($1) > 18)?"$2":"-$2")');
 
 	echo "Fichier : $file"
 	echo "Dates :  $DATES"
+	echo
+	echo "Ancien solde : $ANCIEN_SOLDE"
+	echo "Nouveau solde : $SOLDE"
 	echo
 	echo 'Debits : '
 	echo "$DEBITS"
